637d64fb6185723eb63f06ce47906aae57516e12
Yakiv87 <130331039+Yakiv87@users.noreply.github.com>
2023-09-16 23:57:35 -0400

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,60 @@
 // Add imports above this line
+
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
 // Change code below this line
 
+const galleryList = document.querySelector('.gallery');
+const galleryItemMarkup = createGalleryMarkup(galleryItems);
+galleryList.insertAdjacentHTML("beforeend", galleryItemMarkup);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+  .map(({
+    preview, original, description}) => { 
+      return `
+  <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+  }).join("");
+}
+galleryList.addEventListener('click', onGalleryItemClick);
+
+function onGalleryItemClick(evt) {
+  evt.preventDefault();
+  
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+  
+  const instance = basicLightbox.create(`
+    <img src="" width="800" height="600">
+  `, {
+    onShow: (instance) => {
+      window.addEventListener('keydown', onEscapeKeyPress);
+    },
+    onClose: (instance) => {
+      window.removeEventListener('keydown', onEscapeKeyPress);
+    }
+  });
+  
+  instance.show();
+
+  const largeImageURL = evt.target.dataset.source;
+  instance.element().querySelector('img').src = largeImageURL;
+
+   function onEscapeKeyPress(evt) {
+    if (evt.code === 'Escape') {
+      instance.close();
+     }
+  }
+}
 console.log(galleryItems);
